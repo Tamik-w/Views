e1bef1aa3928d93d2d36bf9f500560f043962e0d
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-02-25 15:40:39 +0300

    step

--- SRC/JS/app.js
+++ SRC/JS/app.js
@@ -1,5 +1,11 @@
 const countEl = documentElementById('count');
 
+updateVisitCount();
+
 function updateVisitCount(){
-    fetch('')
+    fetch('https://api.countapi.xyz/create?namespace=https://tamik-w.github.io/Views/$enable_reset=1')
+    .then(res => res.json())
+    .then(res => {
+        countEl.innerHTML = res.value;
+    });
 }
--- SRC/JS/dist/app.dev.js
+++ SRC/JS/dist/app.dev.js
@@ -1,7 +1,12 @@
 "use strict";
 
 var countEl = documentElementById('count');
+updateVisitCount();
 
 function updateVisitCount() {
-  fetch('');
+  fetch('https://api.countapi.xyz/create?namespace=https://tamik-w.github.io/Views/$enable_reset=1').then(function (res) {
+    return res.json();
+  }).then(function (res) {
+    countEl.innerHTML = res.value;
+  });
 }
